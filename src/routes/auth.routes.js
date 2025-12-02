653391d7e846a665708474ee0d4a304c8f601bbc
zahirRC28 <zahirvalentino2812@gmail.com>
2025-12-02 15:54:10 +0100

    ADD: correcciones

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const { registarUsuario, loginUsuario, renovarToken } = require('../controllers/auth.controllers');
+const { registarUsuario, loginUsuario, renovarToken } = require('../controllers/auth.controller');
 const { validarInput } = require('../middlewares/validarInput');
 const { verificarJWT } = require('../middlewares/validarJWT');
 
@@ -18,8 +18,7 @@ router.post('/signup', [
         .isEmail().withMessage("Escriba un correo electrónico válido.").bail()
         .isLength({ min: 5, max: 50 }).withMessage("Escriba un email válido válido"),
     check("contrasena", "La contraseña debe tener entre 6 y 10 caracteres, contener por lo menos una minúscula, una mayúscula, un número y un símbolo.")
-        .isStrongPassword({ minLength: 6 }).bail()
-        .isLength({ max: 10 }),
+        .isStrongPassword({ minLength: 6 }).bail(),
     validarInput], registarUsuario);
 
 //Login de usuario:
--- tablas.sql
+++ tablas.sql
@@ -5,9 +5,9 @@ DROP TABLE peliculas;
 CREATE TABLE usuarios (
     id_usuario SERIAL PRIMARY KEY,
     nombre_usuario VARCHAR(50) NOT NULL,
-    role_usuario VARCHAR(20) NOT NULL,
+    role_usuario VARCHAR(20) DEFAULT('user') NOT NULL ,
     email VARCHAR(50) UNIQUE NOT NULL,
-    contrasena TEXT NOT NULL,
+    contrasena VARCHAR(50) NOT NULL,
     CONSTRAINT ck_role_usuario CHECK (role_usuario IN ('user', 'administrador'))
 );
 
