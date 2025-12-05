6cb38625aaa13f2b2411af75ac1881ee74d426c8
HannaPolishchuk <hanna.polishchuk@gmail.com>
2025-12-05 11:44:49 +0100

    UPDATE: set cors to accept our localhost  URL.

--- src/app.js
+++ src/app.js
@@ -6,8 +6,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4001;
 const frontendUrl = 'https://movie-app-ne82.onrender.com';
+const localUrl = 'http://localhost:3002';
+const whitelist = [frontendUrl, localUrl];
 const corsOptions = {
-    origin: frontendUrl,
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Esta conexión no está permitida por CORS'));
+        }
+    },
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
